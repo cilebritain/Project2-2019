fc8fe76bd4f4ec5ffa94a76f13ecff5f0f7c5fe0
cilebritain <2023697797@qq.com>
2019-06-22 23:36:42 +0800

    6.22 3
    解决cart编号问题
    下一步解决不能反复加入购物车

--- php/addtocart.php
+++ php/addtocart.php
@@ -1,9 +1,9 @@
 <?php
     $mysqli=new mysqli('localhost','root','r00t','db_project2');
-    $sql1='SELECT count(*) FROM carts';
-    $result1=$mysqli->query($sql1);
-    $rows=mysqli_fetch_row($result1);
-    $row=$rows[0];
+    $sql1='SELECT * FROM carts ORDER BY cartID DESC LIMIT 0,1';
+    $row=mysqli_fetch_object($mysqli->query($sql1))->cartID;
+    //$row=mysqli_fetch_row($result1);
+    //$row=$rows[0];
     $sql2='SELECT userID FROM users WHERE name="'.$_COOKIE["user"].'"';
     $result2=$mysqli->query($sql2);
     $uid=mysqli_fetch_object($result2)->userID;
